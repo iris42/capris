8689cff0e6bc57b366406d13e1da09e677a9742c
Eugene Eeo <xe0jun@hotmail.com>
2014-06-24 21:16:04 +0800

    website

--- commandeer/utils.py
+++
@@ -1,9 +0,0 @@
-def make_options(options):
-    results = {}
-    for key, value in options.items():
-        key = key.replace('_', '-')
-        option = '--{key}'.format(key=key)
-        if len(key) == 1:
-            option = '-{key}'.format(key=key)
-        results[option] = value
-    return results
