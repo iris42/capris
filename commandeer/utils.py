5dea51467523ba6f520c536887312c7121ac196f
Eugene Eeo <xe0jun@hotmail.com>
2014-06-24 22:52:30 +0800

    move subsitute_values function into utils module

--- commandeer/utils.py
+++ commandeer/utils.py
@@ -1,3 +1,21 @@
+from re import compile
+
+regex = compile(r'\$(\{[a-zA-Z.]+\}|[a-zA-Z.]+)(?=[^\']*(?:\'[^\']*\'[^\']*)*$)')
+
+def substitute_values(string, values, default=''):
+    def callback(m):
+        key = m.group(1).strip('{').strip('}')
+        return fetch_value(values, key, default)
+    return regex.sub(callback, string)
+
+def fetch_value(values, key, default):
+    keys = key.split('.')
+    for item in keys:
+        if item not in values:
+            return default
+        values = values[item]
+    return values
+
 def make_options(options):
     results = {}
     for key, value in options.items():
