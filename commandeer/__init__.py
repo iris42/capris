985d33dcbc3b941eb83fc2447988918c9e6e6f99
Eugene Eeo <xe0jun@hotmail.com>
2014-06-25 15:54:58 +0800

    reduce need for additional import

--- commandeer/__init__.py
+++ commandeer/__init__.py
@@ -1,6 +1,5 @@
 from commandeer.command import Command
 from commandeer.pipe import Pipe
-from commandeer.iocontext import IOContext
 
 __all__ = ['Command','Pipe']
-VERSION='0.0.27'
+VERSION='0.0.28'
