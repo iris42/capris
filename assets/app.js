302c101f1ce6f6b4858493f4d05e457f141fd13e
Eugene Eeo <xe0jun@hotmail.com>
2014-06-26 22:26:27 +0800

    added nice warnings

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
     container = $('div#container');
     $.ajax({
-        url: 'https://api.github.com/repos/eugene-eeo/commandeer/contents/README.md',
+        url: 'https://api.github.com/repos/eugene-eeo/commandeer/readme',
+        accepts: 'application/vnd.github.v3.raw',
+        dataType: 'text',
         success: function (data) {
-            var content = data.content;
-            container.html(marked(atob(content)));
+            try {
+                var markdown = marked(atob(data.content));
+                container.html(markdown);
+            } catch (err) {
+                container.html("<img src='assets/error.png' align=left>"+
+                               "<p>An error occured. Please try using a different browser "+
+                               "if you are using <b>Safari</b> as the app is known to fail "+
+                               "on <b>Safari</b>.</p>");
+            }
 
             $('pre code').each(function(i, block) {
                 console.log(block);
                 hljs.highlightBlock(block);
             });
+        },
+        error: function() {
+            container.html("<img src='assets/error.png' align=left>" +
+                           "<p><b>An error occured.</b> Please try reloading the app. " +
+                           "If the error persists perhaps you can try contacting the "  +
+                           "maintainer at <a href='mailto:packwolf58@gmail.com'>packwolf58@gmail.com</a></p>");
         }
     });
 });
