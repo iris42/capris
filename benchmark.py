7aac44e035801402959893a22a7e61fca2b67596
Eugene Eeo <xe0jun@hotmail.com>
2014-07-07 13:54:02 +0800

    remove benchmarks

--- benchmark.py
+++
@@ -1,5 +0,0 @@
-from cProfile import run as profile
-from capris.core import run, run_command
-
-profile('run_command(["echo", "pattern"])')
-profile('run([["grep", "pattern"], ["cat"]], data="pattern")')
