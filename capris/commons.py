877171af9676f59631c581d0472235f04f968e8f
Eugene Eeo <xe0jun@hotmail.com>
2014-08-22 16:43:52 +0800

    expose commonly used commands

---
+++ capris/commons.py
@@ -0,0 +1,19 @@
+from capris.command import Command
+
+
+# mucking about with files
+echo = Command('echo')
+cat = Command('cat')
+
+# text manipulation
+grep = Command('grep')
+sort = Command('sort')
+sed = Command('sed')
+awk = Command('awk')
+cut = Command('cut')
+tr = Command('tr')
+
+# version control
+git = Command('git')
+svn = Command('svn')
+hg = Command('hg')
