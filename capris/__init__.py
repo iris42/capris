af066eb710ffdf541f9a06733dfd487cbf3a3286
Eugene Eeo <xe0jun@hotmail.com>
2014-08-23 17:24:50 +0800

    write setup.py file

--- capris/__init__.py
+++ capris/__init__.py
@@ -1,1 +1,3 @@
 from capris.command import Command
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,11 @@
+import capris
+from distutils.core import setup
+
+setup(
+    name='capris',
+    version=capris.__version__,
+    author='Eugene Eeo',
+    author_email='packwolf58@gmail.com',
+    description='Wrapper for doing beautiful commands',
+    packages=['capris'],
+)
